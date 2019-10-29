b40621fdd71e036b6e9b0e012a6b2ffac7cb8cbb
Mindaugas Dudenas <murama@Mindaugass-MacBook-Pro.local>
2019-10-29 15:19:06 +0200

    updated to work with google drive trough collab

--- magenta/video/next_frame_prediction_pix2pix/create_video.sh
+++ magenta/video/next_frame_prediction_pix2pix/create_video.sh
@@ -31,11 +31,11 @@ else
     case "$response" in
         [yY][eE][sS]|[yY])
             echo "creating the 'frames' dir"
-            mkdir -p $1/frames
-            rm -f $1/frames/*.jpg
+            mkdir -p "$1"/frames
+            rm -f "$1"/frames/*.jpg
             python ../tools/extract_frames.py \
-                   --video_in $1/video.mp4 \
-                   --path_out $1/frames
+                   --video_in "$1"/video.mp4 \
+                   --path_out "$1"/frames
             ;;
         *)
             echo "keeping 'frames' folder"
@@ -46,8 +46,8 @@ else
     case "$response" in
         [yY][eE][sS]|[yY])
             echo "copying the test frame"
-            mkdir -p $1/img
-            cp $1/frames/f0000001.jpg $1/img/first.jpg
+            mkdir -p "$1"/img
+            cp "$1"/frames/f0000001.jpg "$1"/img/first.jpg
             ;;
         *)
             echo "keeping test.jpg"
@@ -58,9 +58,9 @@ else
     case "$response" in
         [yY][eE][sS]|[yY])
             echo "creating the 'good' dir copying the frame folder"
-            rm -rf $1/good
-            mkdir -p $1/good
-            cp $1/frames/* $1/good
+            rm -rf "$1"/good
+            mkdir -p "$1"/good
+            cp "$1"/frames/* "$1"/good
             ;;
         *)
             echo "keeping 'good' folder"
@@ -71,8 +71,8 @@ else
     case "$response" in
         [yY][eE][sS]|[yY])
             echo "recreate 'train'"
-            rm -rf $1/train
-            mkdir -p $1/train
+            rm -rf "$1"/train
+            mkdir -p "$1"/train
             ;;
         *)
             echo "keeping 'train'"
@@ -83,8 +83,8 @@ else
     case "$response" in
         [yY][eE][sS]|[yY])
             echo "removing logs"
-            rm -rf $1/logs
-            mkdir -p $1/logs
+            rm -rf "$1"/logs
+            mkdir -p "$1"/logs
             ;;
         *)
             echo "keeping logs"
@@ -95,7 +95,7 @@ else
     case "$response" in
         [yY][eE][sS]|[yY])
             echo "removing video"
-            rm -f $4/video*.mp4
+            rm -f "$4"/video*.mp4
             ;;
         *)
             echo "keeping video"
@@ -106,8 +106,8 @@ else
     case "$response" in
         [yY][eE][sS]|[yY])
             echo "removing model checkpoint"
-            rm -f $1/pix2pix.model*
-            rm -f $1/checkpoint
+            rm -f "$1"/pix2pix.model*
+            rm -f "$1"/checkpoint
             ;;
         *)
             echo "keeping model"
@@ -118,21 +118,21 @@ else
     case "$response" in
         [yY][eE][sS]|[yY])
             echo "recreate 'test'"
-            mkdir -p $1/test
-            rm -f $1/test/*.jpg
+            mkdir -p "$1"/test
+            rm -f "$1"/test/*.jpg
             python join_pairs.py \
-                   --path_left $1/frames \
-                   --path_right $1/good \
-                   --path_out $1/val \
+                   --path_left "$1"/frames \
+                   --path_right "$1"/good \
+                   --path_out "$1"/test \
                    --limit 10
 
             echo "recreate 'val'"
-            mkdir -p $1/val
-            rm -f $1/val/*.jpg
+            mkdir -p "$1"/val
+            rm -f "$1"/val/*.jpg
             python join_pairs.py \
-                   --path_left $1/frames \
-                   --path_right $1/good \
-                   --path_out $1/val \
+                   --path_left "$1"/frames \
+                   --path_right "$1"/good \
+                   --path_out "$1"/val \
                    --limit 10
             ;;
         *)
@@ -148,73 +148,73 @@ else
 
         echo "making pairs $i/$2"
         python join_pairs.py \
-                   --path_left $1/frames \
-                   --path_right $1/good \
-                   --path_out $1/train \
+                   --path_left "$1"/frames \
+                   --path_right "$1"/good \
+                   --path_out "$1"/train \
                    --limit 1000
 # 1000 is the default value, you can play with it and will get diferents results
         echo "training $i/$2"
         # main.py belongs to the pix2ix_tensorflow package
         python pix2pix-tensorflow-0.1/main.py \
-               --dataset_path $1 \
-               --checkpoint_dir $1 \
+               --dataset_path "$1" \
+               --checkpoint_dir "$1" \
                --epoch 5 \
                --max_steps 10000 \
                --phase train \
                --continue_train 1
 # 10000 is the default value, you can play with it and will get diferents results
         echo "cleaning logs"
-        rm -f $1/logs/*
+        rm -f "$1"/logs/*
 
         echo "backup model $i"
-        mkdir -p $4/model_$n
-        cp $1/checkpoint $4/model_$n
-        cp $1/pix2pix.model* $4/model_$n
+        mkdir -p "$4"/model_$n
+        cp "$1"/checkpoint "$4"/model_$n
+        cp "$1"/pix2pix.model* "$4"/model_$n
 
         echo "generate video test $i"
-        ./recursion_640.sh $4/model_$n $1/img/first.jpg $3 $4/video_$n
+        ./recursion_640.sh "$4/model_$n" "$1/img/first.jpg" $3 "$4/video_$n"
 
         echo "select some pairs for recursion"
-        rm -rf $1/recur
-        mkdir -p $1/recur
-        python ../tools/random_pick.py --path_in $1/good --path_out $1/recur \
+        rm -rf "$1"/recur
+        mkdir -p "$1"/recur
+        python ../tools/random_pick.py --path_in "$1"/good --path_out "$1"/recur \
                --limit 100
 # 100 is the default value, you can play with it and will get diferents results
         echo "use pre-recursion"
         python pix2pix-tensorflow-0.1/main.py \
-               --checkpoint_dir $1 \
+               --checkpoint_dir "$1" \
                --recursion 15 \
                --phase pre_recursion \
-               --dataset_path $1/recur \
-               --frames_path $1/frames
+               --dataset_path "$1"/recur \
+               --frames_path "$1"/frames
 # 15 is the default value, you can play with it and will get diferents results
         echo "generate pairs from recursion"
         python join_pairs.py \
-               --path_left $1/recur \
-               --path_right $1/good \
-               --path_out $1/train \
+               --path_left "$1"/recur \
+               --path_right "$1"/good \
+               --path_out "$1"/train \
                --prefix pr \
                --size 256
 
         echo "select some pairs for recursion (long)"
-        rm -rf $1/recur
-        mkdir -p $1/recur
-        python ../tools/random_pick.py --path_in $1/good --path_out $1/recur \
+        rm -rf "$1"/recur
+        mkdir -p "$1"/recur
+        python ../tools/random_pick.py --path_in "$1"/good --path_out "$1"/recur \
                --limit 2
 # 2 is the default value, you can play with it and will get diferents results
         echo "use pre-recursion (long)"
         python pix2pix-tensorflow-0.1/main.py \
-               --checkpoint_dir $1 \
+               --checkpoint_dir "$1" \
                --recursion 100 \
                --phase pre_recursion \
-               --dataset_path $1/recur \
-               --frames_path $1/frames
+               --dataset_path "$1"/recur \
+               --frames_path "$1"/frames
 # 100 is the default value, you can play with it and will get diferents results
         echo "generate pairs from recursion (long)"
         python join_pairs.py \
-               --path_left $1/recur \
-               --path_right $1/good \
-               --path_out $1/train \
+               --path_left "$1"/recur \
+               --path_right "$1"/good \
+               --path_out "$1"/train \
                --prefix pr \
                --size 256
 
